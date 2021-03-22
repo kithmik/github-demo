e1674a4e143b4907a173df61f0517ff28147e284
Kithmi Kalpana <37541251+kithmik@users.noreply.github.com>
2021-03-22 15:49:42 +0530

    Add files via upload
    Add math.py

---
+++ math.py
@@ -0,0 +1,21 @@
+def add(x,y):
+    return (x+y)
+
+def subtract(x,y):
+    return (x-y)
+
+def multiply(a,b):
+    return (a*b)
+
+def divide(x,y):
+    return (x/y)
+
+==========
+    if y==0
+       return Divide_by_0Error
+    else:
+       return x/y
+===========Bug123
+
+def squre(x):
+    return x*x
